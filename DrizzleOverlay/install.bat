d166c5636523fe54abaccae637073a55c09c1783
ddb174 <ddb174@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2008-04-15 20:17:41 +0000

    added install.bat
    git-svn-id: https://alcugs.almlys.org/svn/drizzle@1212 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

---
+++ DrizzleOverlay/install.bat
@@ -0,0 +1,40 @@
+@echo off
+
+rem Copyright Dustin Bernard 2008
+rem This file is released under the GPLv3, or (at your option) any later version.
+
+echo Make sure Uru is not running and that you have Path of the Shell or Complete Chronicles installed.  Press ctrl-c to quit or any other key to continue.
+pause
+echo Trying to find POTS folder...
+set urupath=null
+if exist "c:\program files\ubi soft\cyan worlds\uru - ages beyond myst\PrimeToX2.exe" set urupath=c:\program files\ubi soft\cyan worlds\uru - ages beyond myst\
+if exist "c:\program files\ubi soft\cyan worlds\uru - complete chronicles\PrimeToX2.exe" set urupath=c:\program files\ubi soft\cyan worlds\uru - complete chronicles\
+if exist "c:\program files\ubi soft\cyan worlds\myst uru complete chronicles\PrimeToX2.exe" set urupath=c:\program files\ubi soft\cyan worlds\myst uru complete chronicles\
+if exist "d:\program files\ubi soft\cyan worlds\uru - ages beyond myst\PrimeToX2.exe" set urupath=d:\program files\ubi soft\cyan worlds\uru - ages beyond myst\
+if exist "d:\program files\ubi soft\cyan worlds\uru - complete chronicles\PrimeToX2.exe" set urupath=d:\program files\ubi soft\cyan worlds\uru - complete chronicles\
+if exist "d:\program files\ubi soft\cyan worlds\myst uru complete chronicles\PrimeToX2.exe" set urupath=d:\program files\ubi soft\cyan worlds\myst uru complete chronicles\
+if exist "e:\program files\ubi soft\cyan worlds\uru - ages beyond myst\PrimeToX2.exe" set urupath=e:\program files\ubi soft\cyan worlds\uru - ages beyond myst\
+if exist "e:\program files\ubi soft\cyan worlds\uru - complete chronicles\PrimeToX2.exe" set urupath=e:\program files\ubi soft\cyan worlds\uru - complete chronicles\
+if exist "e:\program files\ubi soft\cyan worlds\myst uru complete chronicles\PrimeToX2.exe" set urupath=e:\program files\ubi soft\cyan worlds\myst uru complete chronicles\
+if exist "h:\program files\ubi soft\cyan worlds\uru - ages beyond myst\PrimeToX2.exe" set urupath=h:\program files\ubi soft\cyan worlds\uru - ages beyond myst\
+if exist "h:\program files\ubi soft\cyan worlds\uru - complete chronicles\PrimeToX2.exe" set urupath=h:\program files\ubi soft\cyan worlds\uru - complete chronicles\
+if exist "h:\program files\ubi soft\cyan worlds\myst uru complete chronicles\PrimeToX2.exe" set urupath=h:\program files\ubi soft\cyan worlds\myst uru complete chronicles\
+rem echo path:%urupath%
+if "%urupath%"=="null" goto notfound
+echo Pots found! Press enter to continue.
+pause
+echo Copying files...
+xcopy ..\dat\*.* "%urupath%\dat\*.*"
+xcopy ..\Python\*.* "%urupath%\Python\*.*"
+xcopy ..\SDL\*.* "%urupath%\SDL\*.*"
+xcopy ..\sfx\*.* "%urupath%\sfx\*.*"
+mkdir "%urupath%\volatile"
+xcopy /e ..\volatile\*.* "%urupath%\volatile\*.*"
+xcopy ..\*.* "%urupath%\*.*"
+echo All done!
+goto done
+:notfound
+echo Unable to find Pots folder. You'll have to install manually.
+goto done
+:done
+pause
