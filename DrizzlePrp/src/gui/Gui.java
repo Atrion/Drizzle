25ee743e75745ae4fe1e36fd7d77bb0bcf25d73b
diafero <diafero@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2009-07-10 17:25:47 +0000

    check if the manually given file is actually in a "dat" folder
    git-svn-id: https://alcugs.almlys.org/svn/drizzle@2326 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

--- DrizzlePrp/src/gui/Gui.java
+++ DrizzlePrp/src/gui/Gui.java
@@ -4644,6 +4644,10 @@ private void jButton60ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIR
 private void jButton86ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton86ActionPerformed
     Vector<String> v = new Vector<String>();
     File f = new File(textfieldState1.getText());
+    if (!f.getParentFile().getName().equals("dat")) {
+           m.err("The file you specify must be in a folder called \"dat\"");
+           return;
+    }
     String filename = f.getName();
     String infolder = f.getParentFile().getParent();
     v.add(filename);
