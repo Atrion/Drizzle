c6e15468bd167335e834bd08d508fa9f0b809049
ddb174 <ddb174@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2009-07-16 02:52:05 +0000

    Dataserver mirror; Dataserver generator; CommandLine interface. (Everything in the log just shows up on screen when using the CommandLine interface to Drizzle.)  Included a bat and sh file since the CommandLine interface requires running DrizzlePrp.jar directly rather than through Drizzle.jar.
    git-svn-id: https://alcugs.almlys.org/svn/drizzle@2334 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

--- Drizzle/src/drizzle/Main.java
+++ Drizzle/src/drizzle/Main.java
@@ -15,6 +15,15 @@ public class Main {
     public static void main(String[] args) {
         try
         {
+            /*if(args.length>0)
+            {
+                System.out.println("commandline2!");
+                for(String arg: args)
+                {
+                    System.out.println(arg);
+                }
+            }*/
+
             File file = shared.GetResource.getResourceAsFile("/drizzle/DrizzlePrp.jar", true);
             String[] command = new String[]{
                 "java",
@@ -22,7 +31,22 @@ public class Main {
                 "-jar",
                 file.getAbsolutePath(),
             };
-            Process proc = Runtime.getRuntime().exec(command);
+
+            String[] fullcommand = new String[command.length+args.length];
+            for(int i=0;i<command.length;i++)
+            {
+                fullcommand[i] = command[i];
+            }
+            for(int i=0;i<args.length;i++)
+            {
+                fullcommand[command.length+i] = args[i];
+            }
+            /*System.out.println("commandline3!");
+            for(String arg: fullcommand)
+            {
+                System.out.println(arg);
+            }*/
+            Process proc = Runtime.getRuntime().exec(fullcommand);
             proc.waitFor();
         }
         catch(Exception e)
